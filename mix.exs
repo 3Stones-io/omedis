487055e434c9dd986ea249e4f442f1eca03d0ac7
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2024-09-12 12:59:09 +0300

    Added Credo

--- mix.exs
+++ mix.exs
@@ -60,6 +60,7 @@ defmodule Omedis.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:bandit, "~> 1.5"}
     ]
   end
