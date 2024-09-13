8a7ee5c63debb540d24f2fc3da732b017721988a
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2024-09-13 07:38:41 +0300

    Added Ash Dependancy

--- mix.exs
+++ mix.exs
@@ -54,6 +54,8 @@ defmodule Omedis.MixProject do
        compile: false,
        depth: 1},
       {:swoosh, "~> 1.5"},
+      {:ash, "~> 3.0"},
+      {:picosat_elixir, "~> 0.2"},
       {:finch, "~> 0.13"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
