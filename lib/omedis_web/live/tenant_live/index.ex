0a15b3a10af191111318487ef28144c343a517e2
Kimutai01 <kiprotichkimutai01@gmail.com>
2024-09-16 06:12:12 +0300

    test fix

--- lib/omedis_web/live/tenant_live/index.ex
+++ lib/omedis_web/live/tenant_live/index.ex
@@ -18,9 +18,8 @@ defmodule OmedisWeb.TenantLive.Index do
         id="tenants"
         rows={@streams.tenants}
         row_click={fn {_id, tenant} -> JS.navigate(~p"/tenants/#{tenant}") end}
-        class="min-w-full table-auto"
       >
-        <:col :let={{_id, tenant}} label="Id"><%= tenant.id %></:col>
+        <:col :let={{_id, tenant}} label="Id "><%= tenant.id %></:col>
         <:col :let={{_id, tenant}} label="Name"><%= tenant.name %></:col>
         <:col :let={{_id, tenant}} label="Additional info"><%= tenant.additional_info %></:col>
         <:col :let={{_id, tenant}} label="Street"><%= tenant.street %></:col>
