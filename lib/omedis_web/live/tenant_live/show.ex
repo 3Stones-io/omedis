b982d28a5c0c86898f50be69f4ba7fd5417bc982
Kimutai01 <kiprotichkimutai01@gmail.com>
2024-09-17 13:26:11 +0300

    route fix

--- lib/omedis_web/live/tenant_live/show.ex
+++ lib/omedis_web/live/tenant_live/show.ex
@@ -94,7 +94,7 @@ defmodule OmedisWeb.TenantLive.Show do
       :if={@live_action in [:new, :edit]}
       id="log_category-modal"
       show
-      on_cancel={JS.patch(~p"/log_categories")}
+      on_cancel={JS.patch(~p"/tenant/#{@tenant.id}/log_categories")}
     >
       <.live_component
         module={OmedisWeb.LogCategoryLive.FormComponent}
@@ -103,7 +103,7 @@ defmodule OmedisWeb.TenantLive.Show do
         tenants={@tenants}
         action={@live_action}
         log_category={@log_category}
-        patch={~p"/log_categories"}
+        patch={~p"/tenants/#{@tenant.id}/log_categories"}
       />
     </.modal>
     """
