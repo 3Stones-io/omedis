b9d93c28b0f7a629f1f63fc91ea892e591e6d76a
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2024-09-17 16:43:26 +0300

    Added Language Plug

--- lib/omedis_web/router.ex
+++ lib/omedis_web/router.ex
@@ -10,6 +10,7 @@ defmodule OmedisWeb.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug :load_from_session
+    plug OmedisWeb.Plugs.Locale
   end
 
   pipeline :api do
