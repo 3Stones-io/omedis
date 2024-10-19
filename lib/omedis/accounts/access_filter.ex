0860e42abfaa41699e8d1af930aa32cfc5d14182
amos-kibet <kibetamos97@gmail.com>
2024-10-19 10:29:11 +0300

    Refactor code

--- lib/omedis/accounts/access_filter.ex
+++ lib/omedis/accounts/access_filter.ex
@@ -4,25 +4,12 @@ defmodule Omedis.Accounts.AccessFilter do
   """
   use Ash.Policy.FilterCheck
 
-  alias Omedis.Accounts.Tenant
-
   def describe(_) do
     "Filtering resources based on user access rights"
   end
 
   def filter(actor, context, _options) do
-    tenant =
-      case context.query.tenant do
-        %Tenant{} = tenant ->
-          tenant
-
-        tenant_id when is_binary(tenant_id) ->
-          tenant_id = String.replace(context.query.tenant, "tenant_", "")
-          Ash.get(Tenant, tenant_id)
-
-        _ ->
-          nil
-      end
+    tenant = context.subject.tenant
 
     case tenant do
       nil ->
