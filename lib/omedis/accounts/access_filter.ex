fc41f0a5a593d298ef589a4c4ab69462cfd9c257
amos-kibet <kibetamos97@gmail.com>
2024-10-24 16:58:27 +0300

    Make code more readable

--- lib/omedis/accounts/access_filter.ex
+++ lib/omedis/accounts/access_filter.ex
@@ -9,23 +9,16 @@ defmodule Omedis.Accounts.AccessFilter do
   end
 
   def filter(nil, _context, _options), do: expr(false)
+  def filter(_actor, %{subject: %{tenant: nil}}, _options), do: expr(false)
 
-  def filter(actor, context, _options) do
-    tenant = context.subject.tenant
-
-    case tenant do
-      nil ->
-        expr(false)
-
-      tenant ->
-        expr(
-          (exists(
-             access_rights,
-             tenant_id == ^tenant.id and
-               read == true and
-               exists(group.group_users, user_id == ^actor.id)
-           ) and exists(tenant, id == ^tenant.id)) or tenant.owner_id == ^actor.id
-        )
-    end
+  def filter(actor, %{subject: %{tenant: tenant}}, _options) do
+    expr(
+      (exists(
+         access_rights,
+         tenant_id == ^tenant.id and
+           read == true and
+           exists(group.group_users, user_id == ^actor.id)
+       ) and exists(tenant, id == ^tenant.id)) or tenant.owner_id == ^actor.id
+    )
   end
 end
