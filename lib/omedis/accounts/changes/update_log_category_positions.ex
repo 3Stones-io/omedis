e92c7794b929b3b2981e92c7a84fe93e0db01e69
kagure-nyakio <kagurenyakio@gmail.com>
2024-11-04 20:07:32 +0300

    Add access policies to log categories

--- lib/omedis/accounts/changes/update_log_category_positions.ex
+++ lib/omedis/accounts/changes/update_log_category_positions.ex
@@ -7,7 +7,10 @@ defmodule Omedis.Accounts.Changes.UpdateLogCategoryPositions do
   alias Omedis.Accounts.LogCategory
 
   @impl true
-  def change(changeset, _opts, _context) do
+  def change(changeset, _opts, context) do
+    actor = context.actor
+    tenant = context.tenant
+
     Ash.Changeset.before_action(changeset, fn changeset ->
       requested_position = Ash.Changeset.get_attribute(changeset, :position)
       old_position = changeset.data.position
@@ -15,12 +18,14 @@ defmodule Omedis.Accounts.Changes.UpdateLogCategoryPositions do
       max_position =
         LogCategory
         |> Ash.Query.filter(group_id == ^Ash.Changeset.get_attribute(changeset, :group_id))
-        |> Ash.count!()
+        |> Ash.count!(actor: actor, tenant: tenant)
 
       new_position = min(requested_position, max_position)
 
-      with %Ash.BulkResult{status: :success} <- shift_down(old_position, new_position),
-           %Ash.BulkResult{status: :success} <- shift_up(old_position, new_position) do
+      with %Ash.BulkResult{status: :success} <-
+             shift_down(old_position, new_position, tenant, actor),
+           %Ash.BulkResult{status: :success} <-
+             shift_up(old_position, new_position, tenant, actor) do
         changeset
       else
         %Ash.BulkResult{errors: errors} -> Ash.Changeset.add_error(changeset, errors)
@@ -30,15 +35,15 @@ defmodule Omedis.Accounts.Changes.UpdateLogCategoryPositions do
     end)
   end
 
-  defp shift_down(old_position, new_position) do
+  defp shift_down(old_position, new_position, tenant, actor) do
     LogCategory
     |> Ash.Query.filter(position > ^old_position and position <= ^new_position)
-    |> Ash.bulk_update!(:decrement_position, %{}, strategy: :stream)
+    |> Ash.bulk_update!(:decrement_position, %{}, strategy: :stream, actor: actor, tenant: tenant)
   end
 
-  defp shift_up(old_position, new_position) do
+  defp shift_up(old_position, new_position, tenant, actor) do
     LogCategory
     |> Ash.Query.filter(position < ^old_position and position >= ^new_position)
-    |> Ash.bulk_update!(:increment_position, %{}, strategy: :stream)
+    |> Ash.bulk_update!(:increment_position, %{}, strategy: :stream, actor: actor, tenant: tenant)
   end
 end
