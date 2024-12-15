3ec9a05a876fd6bf46403a45462e9607132fa74a
kagure-nyakio <kagurenyakio@gmail.com>
2024-12-15 05:06:31 +0300

    Remove duplicate link

--- lib/omedis_web/components/general_components.ex
+++ lib/omedis_web/components/general_components.ex
@@ -160,28 +160,6 @@ defmodule OmedisWeb.GeneralComponents do
           <ul role="list" class="flex flex-1 flex-col gap-y-7">
             <li>
               <ul role="list" class="-mx-2 space-y-1">
-                <li>
-                  <a
-                    href="#"
-                    class="group flex gap-x-3 rounded-md bg-gray-800 p-2 text-sm font-semibold leading-6 text-white"
-                  >
-                    <svg
-                      class="h-6 w-6 shrink-0"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                      />
-                    </svg>
-                    <%= dgettext("navigation", "Today's Time Tracker") %>
-                  </a>
-                </li>
                 <.organisation_link
                   current_organisation={@current_organisation}
                   current_user={@current_user}
@@ -547,28 +525,6 @@ defmodule OmedisWeb.GeneralComponents do
           <ul role="list" class="flex flex-1 flex-col gap-y-7">
             <li>
               <ul role="list" class="-mx-2 space-y-1">
-                <li>
-                  <a
-                    href="#"
-                    class="group flex gap-x-3 rounded-md bg-gray-800 p-2 text-sm font-semibold leading-6 text-white"
-                  >
-                    <svg
-                      class="h-6 w-6 shrink-0"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                      />
-                    </svg>
-                    <%= dgettext("navigation", "Today's Time Tracker") %>
-                  </a>
-                </li>
                 <.organisation_link
                   current_organisation={@current_organisation}
                   current_user={@current_user}
