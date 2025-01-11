6c4c1cf4dae43e9cc19fffc2f67539f503275669
briankariuki <briankariukidev@gmail.com>
2025-01-11 17:59:56 +0300

    reorder by id

--- lib/omedis/groups.ex
+++ lib/omedis/groups.ex
@@ -7,20 +7,20 @@ defmodule Omedis.Groups do
     resource Omedis.Groups.Group do
       define :create_group, action: :create
       define :destroy_group, action: :destroy
-      define :update_group, action: :update
       define :get_group_by_id, get_by: [:id], action: :read
+      define :get_group_by_slug, get_by: [:slug], action: :read
 
       define :get_group_by_organisation_id,
         action: :by_organisation_id
 
-      define :get_group_by_slug, get_by: [:slug], action: :read
       define :get_groups, action: :read
+      define :update_group, action: :update
     end
 
     resource Omedis.Groups.GroupMembership do
       define :create_group_membership, action: :create
-      define :get_group_membership_by_id, get_by: [:id], action: :read
       define :destroy_group_membership, action: :destroy
+      define :get_group_membership_by_id, get_by: [:id], action: :read
       define :get_group_memberships, action: :read
     end
   end
