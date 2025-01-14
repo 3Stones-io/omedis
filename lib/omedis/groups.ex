44a3ed5776307e74a8ef4ca358f96eb17c0ecd36
briankariuki <briankariukidev@gmail.com>
2025-01-14 11:56:04 +0300

    re-order group resource code interfaces

--- lib/omedis/groups.ex
+++ lib/omedis/groups.ex
@@ -8,10 +8,11 @@ defmodule Omedis.Groups do
       define :create_group, action: :create
       define :destroy_group, action: :destroy
       define :get_group_by_id, get_by: [:id], action: :read
-      define :get_group_by_slug, get_by: [:slug], action: :read
 
       define :get_group_by_organisation_id,
         action: :by_organisation_id
+
+      define :get_group_by_slug, get_by: [:slug], action: :read
 
       define :get_groups, action: :read
       define :latest_group_by_organisation_id, action: :latest_by_organisation_id
