0772c6c825499aa93cc6b78a82984473308c3b56
briankariuki <briankariukidev@gmail.com>
2025-01-17 10:40:58 +0300

    fix login button text

--- lib/omedis_web/live/login_live.ex
+++ lib/omedis_web/live/login_live.ex
@@ -127,7 +127,7 @@ defmodule OmedisWeb.LoginLive do
 
               <div class="mt-6 flex items-center justify-end gap-x-6">
                 {submit(
-                  dgettext("auth", "Signing in..."),
+                  dgettext("auth", "Sign in"),
                   phx_disable_with: dgettext("auth", "Signing in..."),
                   class:
                     "rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
